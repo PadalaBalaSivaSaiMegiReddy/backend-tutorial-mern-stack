1e63bbae2e82fdeb1b398b10a507b9b945527382
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-02-19 17:21:10 +0530

    Add express server and listen for connections

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 const mongoose=require('mongoose')
+const express =require('express')
+const app=express()
 
-mongoose.connect("mongodb+srv://megi:megi123@cluster0.plqgahu.mongodb.net/?retryWrites=true&w=majority").then(()=>console.log("Connected to MongoDB"))
+mongoose.connect("mongodb+srv://megi:megi123@cluster0.plqgahu.mongodb.net/?retryWrites=true&w=majority").then(()=>{console.log("Connected to MongoDB");app.listen(5000,(req,res)=>res.send("Welcome to the server"))}
+).catch((e)=>console.log(e))
