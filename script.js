a87fc71c78a915d0844bc9a4a6b7f3fe09df027a
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-02-19 17:14:30 +0530

    Add MongoDB connection to script.js

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-console.log("running fine")
+const mongoose=require('mongoose')
+
+mongoose.connect("mongodb+srv://megi:megi123@cluster0.plqgahu.mongodb.net/?retryWrites=true&w=majority").then(()=>console.log("Connected to MongoDB"))
