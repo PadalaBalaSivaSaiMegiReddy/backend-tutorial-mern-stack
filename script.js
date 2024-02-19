8ba2f9931c8631a1cdba6158dc7992bf4f7728ca
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-02-19 17:22:38 +0530

    Refactor MongoDB connection and server listening in script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const mongoose=require('mongoose')
 const express =require('express')
 const app=express()
 
-mongoose.connect("mongodb+srv://megi:megi123@cluster0.plqgahu.mongodb.net/?retryWrites=true&w=majority").then(()=>{console.log("Connected to MongoDB");app.listen(5000,(req,res)=>res.send("Welcome to the server"))}
+mongoose.connect("mongodb+srv://megi:megi123@cluster0.plqgahu.mongodb.net/?retryWrites=true&w=majority").then(()=>{console.log("Connected to MongoDB");app.listen(5000,()=>console.log("Welcome to the server "+5000))}
 ).catch((e)=>console.log(e))
