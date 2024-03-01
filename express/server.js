b2db1e63d05ae036dd046785a476eed80f365df2
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-03-01 15:07:05 +0530

    Add user router and pass text to index.ejs template

--- express/server.js
+++ express/server.js
@@ -5,8 +5,12 @@ app.set('view engine','ejs')
 
 app.get('/',(req,res)=>{
     console.log("App get success");
-    res.render("index")
+    res.render("index",{text:"world"})
 })
+
+const userRouter=require('./routes/users')
+
+app.use('/users',userRouter)
 
 app.listen(3000,()=>{
     console.log("App listening to port 3000");
