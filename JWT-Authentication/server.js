21e1d02eb94544aa46f2cf771179cb5a30df2f9b
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-04-04 22:23:15 +0530

    Add dev2 script and update requests.rest and server.js

--- JWT-Authentication/server.js
+++ JWT-Authentication/server.js
@@ -2,39 +2,46 @@ const express = require("express");
 const jwt = require("jsonwebtoken");
 const app = express();
 
+require("dotenv").config();
+
 app.use(express.json());
 
+const posts = [
+  {
+    username: "Megi",
+    title: "Post 1",
+  },
+  {
+    username: "Sita",
+    title: "Post 2",
+  },
+  {
+    username: "Jesse",
+    title: "Post 3",
+  },
+];
 
+app.get("/posts",authenticateToken, (req, res) => {
+  res.json(posts);
+});
 
-const posts =[
-    {
-        username:"Megi",
-        title:"Post 1"
-    },
-    {
-        username:"Sita",
-        title:"Post 2"
-    },
-    {
-        username:"Jesse",
-        title:"Post 3"
-    },
-]
+app.post("/login", (req, res) => {
+  //Authenticate User
 
-app.get("/posts",(req,res)=>{
-    res.json(posts)
-})
+  const username = req.body.username;
 
-app.post('/login',(req,res)=>{
-    //Authenticate User
+  const user = {
+    name: username,
+  };
 
+  const accessToken = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET);
+  res.json({accessToken:accessToken});
+});
 
-    const username = req.body.username
+function authenticateToken(req,res,next){
 
-    
-    
-})
+}
 
-app.listen("3000",()=>{
-    console.log("listening")
-})
+app.listen(3000, () => {
+    console.log("listening server1 on port 3000");
+});
